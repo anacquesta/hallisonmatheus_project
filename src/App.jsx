4c50a96cb1bbe18327c05b2ce053d3b32e6678a7
CarolGonzaga <dev.carolgonzaga@gmail.com>
2026-05-27 08:00:53 -0300

    fix: replace radix toaster with sonner toaster in App.jsx to display notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from 'sonner'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -118,7 +118,7 @@ function App() {
         <Router>
           <AuthenticatedApp />
         </Router>
-        <Toaster />
+        <Toaster richColors position="top-right" />
       </QueryClientProvider>
     </AuthProvider>
   )
